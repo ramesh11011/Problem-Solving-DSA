59553377f33bef654bbb4f9a286338929059d0ad
P Ramesh <162275007+ramesh11011@users.noreply.github.com>
2025-06-05 09:56:45 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (83.51%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> lastIndex(256, -1); 
+        int left = 0;
         int maxLength = 0;
-        int start = 0; // left boundary of the window
 
-        for (int end = 0; end < s.length(); end++) {
-            char currentChar = s[end];
+        vector<int> Hash(256,-1);
 
-            // If the character was seen before and is within the current window
-            if (lastIndex[currentChar] >= start) {
-                start = lastIndex[currentChar] + 1;
+        for(int i = 0; i < s.size(); i++){
+            char charlength = s[i];
+
+            if(Hash[charlength] >= left){
+                left = Hash[charlength]+1;
+
             }
 
-            // Update last seen index of the current character
-            lastIndex[currentChar] = end;
-
-            // Calculate window length
-            maxLength = max(maxLength, end - start + 1);
+            Hash[charlength] = i;
+    
+            maxLength = max(maxLength,i - left + 1 );
         }
 
         return maxLength;
+
+        
+
+
+        
     }
 };
